844a162e2cf88af55d9c33cdc121e1ebb1b3c3a4
Eugene Tolbakov <eugene.tolbakov@dataart.com>
2018-06-21 20:32:55 +0300

    API-4 update hash-map implementation; add uuid as a id

--- src/api_test/handler.clj
+++ src/api_test/handler.clj
@@ -11,43 +11,56 @@
 (def bill  {:id 2 :name "Bill"  :age 28 :salary 8885 :company "Winn"})
 (def jeff  {:id 3 :name "Jeff"  :age 31 :salary 9955 :company "Amma"})
 (def employees (atom #{steve bill jeff}))
-(def employeesMap {:1 steve :2 bill :3 jeff})
+
+(defn uuid []
+  (str (java.util.UUID/randomUUID)))
+
+(def u_steve {:id (uuid) :name "Steve" :age 24 :salary 7886 :company "Acme"})
+(def u_bill  {:id (uuid) :name "Bill"  :age 28 :salary 8885 :company "Winn"})
+(def u_jeff  {:id (uuid) :name "Jeff"  :age 31 :salary 9955 :company "Amma"})
+(def u_employeesMap (atom {(:id u_steve) u_steve (:id u_bill) u_bill (:id u_jeff) u_jeff}))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World!")
-  (GET "/employees-old" [] (response @employees))
-  (GET "/employees" [] (response (vals employeesMap)))
-  ;(GET ["/employees/:id", :id #"[0-9]+"] [id] (response (first (filter #(= (:id %) (Integer/parseInt id)) @employees))))
-  (GET ["/employees/:id", :id #"[0-9]+"] [id] (response (get employeesMap
-                                                             (keyword id)))))
-  ;(GET "/employees/" [param val]
-  ;  (response (if (.contains ["name", "company"] param)
-  ;              (first (filter #(.equals (.toLowerCase ((keyword param) %)) val) @employees))
-  ;              (first (filter #(.equals ((keyword param) %) (Long/parseLong val)) @employees)))))
+           (GET "/" [] "Hello World!")
+           (GET "/employees" [] (response @employees))
+           ;new get-all
+           (GET "/employees-new" [] (response (vals @u_employeesMap)))
 
+           (GET ["/employees/:id", :id #"[0-9]+"] [id] (response (first (filter #(= (:id %) (Integer/parseInt id)) @employees))))
+           ;new get-by-id
+           (GET ["/employees-new/:id"] [id] (response (get @u_employeesMap id)))
 
-           ;(POST "/employees" request
-  ;  (let [person {:id      (inc (apply max (map #(:id %) @employees)))
-  ;                :name    (get-in request [:body :name])
-  ;                :age     (get-in request [:body :age])
-  ;                :salary  (get-in request [:body :salary])
-  ;                :company (get-in request [:body :company])}]
-  ;    (swap! employees conj person)
-  ;    {:status 200}))
-(POST "/employees" request
-  (let [person (-> (:body request)
-                   (assoc :id (inc (apply max (map #(:id %) @employees)))))]
-    (swap! employees conj person)
-    {:status 200}))
+           (GET "/employees/" [param val]
+             (response (if (.contains ["name", "company"] param)
+                         (first (filter #(.equals (.toLowerCase ((keyword param) %)) val) @employees))
+                         (first (filter #(.equals ((keyword param) %) (Long/parseLong val)) @employees)))))
+           (POST "/employees" request
+             (let [person {:id      (inc (apply max (map #(:id %) @employees)))
+                           :name    (get-in request [:body :name])
+                           :age     (get-in request [:body :age])
+                           :salary  (get-in request [:body :salary])
+                           :company (get-in request [:body :company])}]
+               (swap! employees conj person)
+               {:status 200}))
+           ;new post
+           (POST "/employees-new" request
+             (let [person (-> (:body request)
+                              (assoc :id (uuid)))]
+               ;(swap! (into u_employeesMap {(:id person) person}))
+               (swap! u_employeesMap conj {(:id person) person})
+               {:status 200}))
 
-  ;(DELETE ["/employees/:id", :id #"[0-9]+"] [id]
-  ;  (let [person (first (filter #(= (:id %) (Integer/parseInt id)) @employees))]
-  ;    (swap! employees disj person)
-  ;    {:status 200}))
-(DELETE ["/employees/:id", :id #"[0-9]+"] [id]
-    (dissoc employeesMap (keyword id) {:status 200}))
-(route/resources "/")
-(route/not-found "Not Found")
+           (DELETE ["/employees/:id", :id #"[0-9]+"] [id]
+             (let [person (first (filter #(= (:id %) (Integer/parseInt id)) @employees))]
+               (swap! employees disj person)
+               {:status 200}))
+
+           ;new delete
+           (DELETE ["/employees-new/:id"] [id]
+             (swap! u_employeesMap dissoc id))
+
+           (route/resources "/")
+           (route/not-found "Not Found"))
 
 (def app
   (-> (handler/site app-routes)
